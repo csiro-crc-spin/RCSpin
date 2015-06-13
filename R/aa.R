0d1c54750cef9945acd2d67e0e71416e995c2700
dun280 <rob.dunne@csiro.au>
2015-06-13 16:50:25 +1000

    just symptomatic i.e. neither NBCSP nor gemini

--- R/aa.R
+++ R/aa.R
@@ -1906,8 +1906,10 @@ DukesCrcSpinModel <- setRefClass( "DukesCrcSpinModel",
             #       }
             treatment_record.1 <- testForAndTreatCRC(person)
 
-            treatment_record.2<-NBCSP(person)
+#            treatment_record.2<-NBCSP(person)
 #            treatment_record.2<-gemini.screening(person)
+             treatment_record.2<-rep(0,14)
+
 
             return(c(treatment_record.1,treatment_record.2))
         },
