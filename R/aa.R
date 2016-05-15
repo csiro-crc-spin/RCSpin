21e217454c37012c58d4ac4bd8fa7dc164b45970
dun280 <rob.dunne@csiro.au>
2016-05-15 11:08:39 +1000

    added  treatment_record.2<-rep(0,14) -- do I need this

--- R/aa.R
+++ R/aa.R
@@ -1914,9 +1914,9 @@ DukesCrcSpinModel <- setRefClass( "DukesCrcSpinModel",
             #       }
             treatment_record.1 <- testForAndTreatCRC(person)
 
-            treatment_record.2<-NBCSP(person)
+#            treatment_record.2<-NBCSP(person)
 #            treatment_record.2<-gemini.screening(person)
-
+            treatment_record.2<-rep(0,14)
             return(c(treatment_record.1,treatment_record.2))
         },
 
